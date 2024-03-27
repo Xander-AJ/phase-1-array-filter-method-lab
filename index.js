7264a8f7d5c721076acd260d5fc6405fb47fb574
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-03-27 11:10:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+// Helper function to find matching drivers
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Helper function to find drivers with names that begin with provided letters
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Helper function to match drivers based on exact name
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+};
